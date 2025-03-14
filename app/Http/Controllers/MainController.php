70c45950f85fa9b53600de5bb5d207978cbd076b
João Paulo Leite Costa <joaopaulotds@gmail.com>
2025-03-13 23:38:26 -0300

    Loading user notes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index() {
-        echo "I'm inside the app!";
+        //load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        //show home view
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote() {
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::middleware([CheckIsNotLogged::class])->group(function() {
 
 //auth routes - logged
 Route::middleware([CheckIsLogged::class])->group(function() {
-    Route::get('/', [MainController::class, 'index']);
-    Route::get('/newnote', [MainController::class, 'newNote']);
-    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('/', [MainController::class, 'index'])->name('home');
+    Route::get('/newnote', [MainController::class, 'newNote'])->name('new');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 });
